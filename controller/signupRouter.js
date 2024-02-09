f261ad79da05f62ac4f5f9cae04f158c47c500b2
aleenagithub <aleenashijokaitharath@gmail.com>
2024-02-09 11:08:37 +0530

    blog sign

--- controller/signupRouter.js
+++ controller/signupRouter.js
@@ -13,16 +13,8 @@ router.post("/add",async(req,res)=>{
     )
 })
 
-router.get("/view",async(req,res)=>{
-    let data=await signupModel.find()
-    res.json(data)
-})
 
-router.post("/search",async(req,res)=>{
-    let input=req.body
-    let data=await patientModel.find(input)
-   res.json(data)
- })
+
   
 module.exports=router
 
