297e351f216528650034aa07a0c659cd27dad47e
aleenagithub <aleenashijokaitharath@gmail.com>
2024-02-09 21:15:35 +0530

    signin completed

--- controller/signupprouter.js
+++ controller/signupprouter.js
@@ -6,14 +6,14 @@ const signupModel = require("../model/signupModel")
 
 hashPasswardGenertor=async (pass)=>{
     const salt= await bcrypt.genSalt(10)
-    return bcrypt.hash(pass,salt)
+    return bcrypt.hash(pass,salt) 
 
 }
 
 router.post("/add",async(req,res)=>{
     let {data}={"data":req.body}
     let Password=data.Password
-    hashPasswardGenertor(Password).then(
+    /*hashPasswardGenertor(Password).then(
         (hashedPassword)=>{
             console.log(hashedPassword)
             data.Password=hashedPassword
@@ -30,6 +30,58 @@ router.post("/add",async(req,res)=>{
 
 
   
+router.post("/signin",async(req,res)=>{
+    let input =req.body
+    let data=await signupModel.find(input)
+    res.json({
+
+          status:"success"
+    })*/
+    const hashedPassword=await hashPasswardGenertor(Password)
+data.Password=hashedPassword
+let user=new signupModel(data)
+let result=await user.save(data)
+res.json({
+
+    status:"success"
+})
+})
+
+router.post("/signin",async(req,res)=>{
+    let input =req.body
+    let Email=req.body.Email
+//reetrive all info of emailid
+
+    let data=await signupModel.findOne({"Email":Email})
+    if(!data)
+    {
+        return res.json({
+
+            status:"invalid user"
+        })
+    }
+    console.log(data)
+    let dbPassword=data.Password
+    let inputPassword=req.body.Password
+    console.log(dbPassword)
+    console.log(inputPassword)
+    const match=await bcrypt.compare(inputPassword,dbPassword)
+    if(!match){
+        return res.json ({
+
+            status:"invalid password"
+        })
+    }
+    else{
+        res.json(
+            {
+                status:"success"
+            }
+        )
+    }
+    
+})
+
 
 
 
